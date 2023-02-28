05f308a2e288fe7d23b706d7a654615a6ed9efee
Niels Brons <git@nielsbrons.nl>
2023-02-28 02:37:45 +0100

    time settings

--- f1data/settings.py
+++ f1data/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+TIME_INPUT_FORMATS = ('%H:%M',)
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
