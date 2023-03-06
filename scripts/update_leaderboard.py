aa2f7985eee91e5179d4e0147c3361cbd5b8cddb
Niels Brons <git@nielsbrons.nl>
2023-03-06 15:34:01 +0100

    Create update_leaderboard.py

---
+++ scripts/update_leaderboard.py
@@ -0,0 +1,9 @@
+from home.models import Races, Events, Circuit, Point, Driver, Result, PointMath
+
+CURRENT_SEASON = 2023
+
+def run():
+  update_leaderboard()
+  
+def update_leaderboard(season=CURRENT_SEASON):
+  pass
